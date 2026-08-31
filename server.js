c5539156b9a558bbb2b889556d98d25483f66471
Abenezer <mathewosa9@gmail.com>
2026-08-31 17:37:32 -0600

    security: add X-Content-Type-Options, X-Frame-Options, and security headers

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-﻿// -- Request Logger --
+﻿// -- Security Headers --
+app.use((_req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=(self)');
+  next();
+});
+
+// -- Request Logger --
 app.use((req, _res, next) => {
   console.log('[' + new Date().toISOString() + '] ' + req.method + ' ' + req.url);
   next();
